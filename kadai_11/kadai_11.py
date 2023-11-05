a4bc29f3f267c09a5e9067248e30b5ac3488ecbd
msugi-sys <149662376+msugi-sys@users.noreply.github.com>
2023-11-05 13:19:30 +0900

    Create kadai_11.py

---
+++ kadai_11/kadai_11.py
@@ -0,0 +1,13 @@
+# 11章_課題_リストの中身を先頭からfor、whileで記載
+
+list = ["水","金","地","火","木","土","天","海","冥"]
+
+# for文で出力
+for i in range(9):
+	print(list[i])
+
+#while文で出力
+i = 0
+while i <= 8:
+	print(list[i])
+	i += 1
