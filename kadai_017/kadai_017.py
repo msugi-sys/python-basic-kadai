3071a8378d145cd01a069f8bc82772a556b08bd7
msugi-sys <149662376+msugi-sys@users.noreply.github.com>
2023-11-08 14:14:54 +0900

    Update kadai_017.py

--- kadai_017/kadai_017.py
+++ kadai_017/kadai_017.py
@@ -19,20 +19,23 @@ class Human:
 		print('名前は',self.name,'です。')
 		print('年齢は',self.age,'です。')
 
-        def check_adult(self):
-	        if self.age >= 20:
-				print(self.name,'は',大人です。')
-			else :
-				print(self.name,'は',大人ではありません。')
+	def check_adult(self):
+		if self.age >= 20:
+			print(self.name,'は大人です。')
+		else:
+			print(self.name,'は大人ではありません。')
 
-i = int(input('登録人数：'))
+# 登録人数を入力
+num_people = int(input('登録人数：'))
 
-for i in range(i):
+# 入力された人数分だけHumanインスタンスを作成
+for i in range(num_people):
 	a = str(input('名前：'))
 	b = int(input('年齢：'))
 	x = Human(a, b)
 	list_human.append(x)
 
-for i in range(i):
-	list_human[i].printinfo()
-	list_human[i].check_adult()
+# リストの各要素に対してprintinfoとchek_adultを呼び出す
+for human in list_human:
+	human.printinfo()
+	human.check_adult()
